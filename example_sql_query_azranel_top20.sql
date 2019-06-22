296929231f54eb70ba0709ee6d360fb62d3306aa
Bamux <bamueq@googlemail.com>
2019-06-22 21:21:40 +0200

    Add files via upload

--- example_sql_query_azranel_top20.sql
+++ example_sql_query_azranel_top20.sql
@@ -1,6 +1,13 @@
-select date, Player.playername, Player.class, Role.rolename, Guild.guildname, dps, hps, thps, aps, time, totaltime from Encounter a
+select Encounterinfo.date,Boss.bossname, Player.playername,Class.class, Role.rolename, Guild.guildname, dps, hps, thps, aps, time, totaltime  from Encounter a
+inner join Encounterinfo on a.encounterid = Encounterinfo.id
 inner join Player on a.playerid = Player.id
+inner join Boss on Encounterinfo.bossid = Boss.id
 inner join Role on a.roleid = Role.id
-inner join Guild on Player.guildid = Guild.id
-where a.bossid = 1 and a.dps = (select b.dps from Encounter b where a.bossid = b.bossid and a.playerid = b.playerid order by b.dps desc limit 1)
-Order by dps desc limit 20
+inner join Guild on Encounterinfo.guildid = Guild.id
+inner join Class on Player.classid = Class.id
+where Encounterinfo.bossid = 1 and a.dps = (
+select dps from Encounter b
+inner join Encounterinfo on b.encounterid = Encounterinfo.id
+where Encounterinfo.bossid = 1 and a.playerid = b.playerid
+Order by b.dps desc limit 1)
+Order by a.dps desc limit 20
